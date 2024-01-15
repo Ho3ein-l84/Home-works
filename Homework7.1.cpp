32185f67ce299c81a067c592e9d20cbcf79c8b06
Ho3ein-l84 <149825782+Ho3ein-l84@users.noreply.github.com>
2024-01-15 23:53:15 +0330

    Add files via upload

---
+++ Homework7.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+main() {
+int m,n;
+float Q;
+cout<<"Please Enter a positive number: ";
+cin>>m;
+cout<<"Please enter a second positive number: ";
+cin>>n;
+while (Q>n){
+	Q=m-n;
+}
+cout<<"Left over Division: "<<Q;
+ }

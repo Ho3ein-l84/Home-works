b77fdc24cddd6ccdc13b63bc7198ad5dbf77fc55
Ho3ein-l84 <149825782+Ho3ein-l84@users.noreply.github.com>
2024-01-16 00:00:13 +0330

    Add files via upload

---
+++ Homework7.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+main() {
+int m,n,X,R;
+cout<<"Please enter a number: ";
+cin>>m;
+cout<<"Please enter a second number: ";
+cin>>n;
+for (X=n;X>0;X--){
+	R=m*m;
+}
+cout<<R;
+ }
